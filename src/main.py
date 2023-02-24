480b894cc1910ad084c2c2d1b29c9cec02aab437
Amin Bashiri <amin.bashiri28@gmail.com>
2023-02-24 12:27:06 -0700

    Add agent status print

--- src/main.py
+++ src/main.py
@@ -27,6 +27,15 @@ if __name__ == "__main__":
         if command == "l" or command == "list":
             for simulator in simulators:
                 print(simulator + ": " + simulators[simulator].status.name)
+                for agent in simulators[simulator].agents:
+                    print(
+                        "    Agent "
+                        + str(agent.id)
+                        + ": "
+                        + agent.status.name
+                        + ", transactions: "
+                        + str(agent.total_transactions)
+                    )
 
     # Stopping threads and ending the program
 
